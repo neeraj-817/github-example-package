b8000465be127d5d3ca02b44d4bb7f90dc01d548
neeraj-817 <104046556+neeraj-817@users.noreply.github.com>
2023-03-26 20:23:45 +0530

    i make one change

--- src/main/java/unitTesting/Testing.java
+++ src/main/java/unitTesting/Testing.java
@@ -8,7 +8,7 @@ package unitTesting;
 public class Testing {
 
     public static void display(String param){
-        System.out.println("You parameter is " + param);
+        System.out.println("You parameter are " + param);
     }
 
     public void someFunc(){
